01b38233647f48c3120588bc303b52c4e036f639
Brandon Thigpen <brandonkthigpen@gmail.com>
2023-12-03 14:36:55 -0500

    Commits Day 2 Part 1 answer

--- app/day2/PartOne.tsx
+++ app/day2/PartOne.tsx
@@ -1,20 +1,53 @@
-
 "use client";
 
-
 export default function PartOne ({ input}: { input: string }) {
+  const RED_CUBES = 12;
+  const BLUE_CUBES = 14;
+  const GREEN_CUBES = 13;
+  
+  const gamesArray = input.split('Game ').filter(game => game !== '');
 
-  const fomatInput = input.split('Game');
+  const result = gamesArray.map((game, i) => {
+    const gameData = game.trim().split('\n');
+    const gameNumber = gameData[0].split(':');
+    const gameValue = gameNumber[1];
 
-  // const test = fomatInput.reduce((acc, curr) => {
-  //   return {
-  //     ...acc,
-  //     curr
-  //   }
+    // muliple rounds per game, so split on the semicolon
+    const gameRoundRaw = gameValue.split('; ').map(round => round.trim());
 
-  // }, []);
-  console.log(fomatInput);
+    const createGameRounds = gameRoundRaw.map(round => {
+      const roundObject: { blue?: number, red?: number, green?: number } = round.split(', ').reduce((acc, curr) => {
+        const splitOnSpace = curr.split(' ');
+        return { ...acc, [splitOnSpace[1]]: parseInt(splitOnSpace[0]) };
+      }, {});
 
-  //
-  return <div>{input}</div>
+      // ensure every round is consistent
+      const formatRoundObject = {
+        blue: roundObject.blue || 0,
+        red: roundObject.red || 0,
+        green: roundObject.green || 0,
+      };
+
+      return formatRoundObject;
+    });
+
+    const isGamePossible = createGameRounds.reduce((acc, curr) => {
+      if (curr.red > RED_CUBES || curr.blue > BLUE_CUBES || curr.green > GREEN_CUBES) {
+        acc = false;
+        return acc;
+      }
+
+      return acc;
+    }, true);
+
+    if (isGamePossible) {
+      return i + 1;
+    }
+
+    return 0;
+  });
+
+  const addAnswer = result.reduce((acc, curr) => acc+ curr, 0);
+
+  return <div>{addAnswer}</div>
 }
--- app/day2/page.tsx
+++ app/day2/page.tsx
@@ -1,4 +1,5 @@
-import testSamplePart1 from "./testSamplePart1";
+// import testSamplePart1 from "./testSamplePart1";
+import puzzleInput from "./puzzleInput";
 
 import PartOne from "./PartOne";
 
@@ -8,8 +9,7 @@ export default function Day2 () {
       <h1 className="text-5xl">Day 2: Cube Conundrum</h1>
       <div className="pt-4">
         <h3 className="text-3xl">Part One</h3>
-        {/* <PartOne input={puzzleInput} /> */}
-        <PartOne input={testSamplePart1} />
+        <PartOne input={puzzleInput} />
       </div>  
     </div>
   );
