218a0b47ca1b5909bed8264192e55655c154c343
Brandon Thigpen <brandonkthigpen@gmail.com>
2023-12-09 12:06:12 -0500

    Fixes typos

--- app/day6/page.tsx
+++ app/day6/page.tsx
@@ -4,10 +4,10 @@ import puzzleInput from "./puzzleInput";
 import Part1 from "./Part1";
 import Part2 from "./Part2";
 
-export default function Day4() {
+export default function Day6() {
   return (
     <div>
-      <h1 className="text-5xl">Day 4: Scratchcards</h1>
+      <h1 className="text-5xl">Day 6: Wait For It</h1>
       <div className="pt-4">
         <h3 className="text-3xl">Part 1</h3>
         <Part1 input={puzzleInput} />
